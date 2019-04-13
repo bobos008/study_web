634c6773fa3dcf6e51cfa740c600e7303c40ccc3
bobos008 <501919337@qq.com>
2019-04-13 15:36:20 +0800

    增加识别手机的中间件

--- lovepig_manager/bookmids/middleware.py
+++ lovepig_manager/bookmids/middleware.py
@@ -1,9 +1,8 @@
 # coding=utf-8
 
 
+from django.http import HttpResponseRedirect 
 from django.utils.deprecation import MiddlewareMixin
-from django.http import JsonResponse, HttpResponseRedirect 
-from django.shortcuts import redirect
 
 
 class Mids(MiddlewareMixin):
@@ -11,11 +10,11 @@ class Mids(MiddlewareMixin):
         ''' 调用视图前识别是手机端，还是pc端 '''
         MOBILE_USER_AGENTS = ['Android', 'iPhone']
         current_user_agent = request.META['HTTP_USER_AGENT']
-        print '------------------------'
-        # return HttpResponseRedirect('/indexs/index')
 
-        # for ua in MOBILE_USER_AGENTS:        
-        #     if ua in current_user_agent:
-        #         return HttpResponseRedirect(reverse('/indexs/mobileIndex/'))
-        # else:
-        #         return HttpResponseRedirect('/indexs/index/')
+        current_url = request.path
+        if current_url != '/indexs/mobileIndex/':
+            for ua in MOBILE_USER_AGENTS:        
+                if ua in current_user_agent:
+                    return HttpResponseRedirect('/indexs/mobileIndex/')
+        else:
+            return 
