3dfec38a0fe0feba4e8df7d09d1e0cd6c8e7d7f4
bobos008 <501919337@qq.com>
2019-04-17 18:55:23 +0800

    前后台交互

--- lovepig_manager/indexs/urls.py
+++ lovepig_manager/indexs/urls.py
@@ -4,11 +4,11 @@
 from django.conf.urls import url, include
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     url(r'^index/', views.index),
     url(r'^advantage/', views.advantage),
     url(r'^brandHistory/', views.brandHistory),
     url(r'^classicCase/', views.classicCase),
     url(r'^corporateNews/', views.corporateNews),
     url(r'^mobileIndex/', views.mobileIndex),
-}
+]
